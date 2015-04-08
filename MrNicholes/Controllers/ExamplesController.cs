68adc78526b740ec7786f6e7833d5f32918f7e86
jnicholes@outlook.com <jnicholes@outlook.com>
2015-04-08 11:41:14 -0500

    removing a bunch of junk

---
+++ MrNicholes/Controllers/ExamplesController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace MrNicholes.Controllers
+{
+    public class ExamplesController : Controller
+    {
+        // GET: Examples
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- MrNicholes/Controllers/HomeController.cs
+++ MrNicholes/Controllers/HomeController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Net;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace MrNicholes.Controllers
 {
@@ -17,47 +12,13 @@ namespace MrNicholes.Controllers
     public ActionResult About()
     {
       ViewBag.Message = "Your application description page.";
-
       return View();
     }
 
     public ActionResult Contact()
     {
       ViewBag.Message = "Your contact page.";
-
       return View();
-    }
-
-    public ActionResult GetHostEntry(string hostname, string dnsServer)
-    {
-      var response = NsLookup(hostname, dnsServer);
-      return Json(response, JsonRequestBehavior.AllowGet);
-    }
-
-    private static IEnumerable<string> NsLookup(string host, string dnsServer, string recordType = "A")
-    {
-      var text = new List<string>();
-      var process = new Process();
-      var startInfo = new ProcessStartInfo("nslookup");
-
-      startInfo.Arguments = "-q=" + recordType + " " + host + " " + dnsServer;
-      startInfo.RedirectStandardOutput = true;
-      startInfo.UseShellExecute = false;
-      startInfo.CreateNoWindow = true;
-      
-      process.StartInfo = startInfo;
-      process.OutputDataReceived += (sender, args) => text.Add(FormatString(args.Data));
-
-      process.Start();
-      process.BeginOutputReadLine();
-      process.WaitForExit();
-
-      return text;
-    }
-
-    private static string FormatString(string input)
-    {
-      return input;
     }
   }
 }
